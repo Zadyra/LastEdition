503ea3c2a4700f5bb51c70e8c092e23079d7d930
Zadyra <alhslux@hotmail.com>
2018-07-13 16:22:32 +0200

    test

--- src/org/betavzw/LijstBestemmingen.java
+++ src/org/betavzw/LijstBestemmingen.java
@@ -6,18 +6,24 @@ public class LijstBestemmingen {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String[] bestemmingen = {"de kust", "de zee", "het strand"};
+        double[] prijzen = {10.00, 15.00, 12.50};
         System.out.print("Geef je naam: ");
         String naam = input.nextLine();
-        System.out.println("Kies een bestemming: ");
-        for (int i = 0; i < bestemmingen.length; i++) {
-            System.out.printf("\t%d. %s\n", i + 1, bestemmingen[i]);
-        }
-        System.out.print("Welke bestemming kies je (1-3)? ");
-        int bestemming = Integer.parseInt(input.nextLine());
+        int bestemming;
+        do {
+            System.out.println("Kies een bestemming: ");
+            for (int i = 0; i < bestemmingen.length; i++) {
+                System.out.printf("\t%d. %s (€%.2f)\n", i + 1, bestemmingen[i], prijzen[i]);
+            }
+            System.out.print("Welke bestemming kies je (1-3)? ");
+            bestemming = Integer.parseInt(input.nextLine());
+            if (bestemming < 0 || bestemming > bestemmingen.length) {
+                System.out.println("Kies een geldig nummer!");
+            }
 
+        } while (bestemming < 1 || bestemming > bestemmingen.length);
 
-        System.out.printf("Dag %s, goede reis naar %s.", naam, bestemmingen[bestemming - 1]);
-
-
+        System.out.printf("Dag %s, goede reis naar %s. Dit kost je €%.2f.", naam, bestemmingen[bestemming - 1],
+                prijzen[bestemming - 1]);
     }
 }
